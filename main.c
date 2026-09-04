12e01c50ea5adc65b2230cb4e14e18ee372d21a1
Cawazanga <alexandrpav6@icloud.com>
2026-09-04 16:56:42 +0300

    если что все ост коммиты тоже мои

--- main.c
+++ main.c
@@ -52,7 +52,7 @@ int main(void) {
         }
         if (buf[0] == 'i' && buf[1] == 'f') {
             wwa(buf, 5); //
-            wwvif(iv, argsu[0][0], atoi(argsu[1]), atoi(argsu[2]), atoi(argsu[3]), atoi(argsu[4]));
+            wwvif(iv, argsu[0][0], atoi(argsu[1]), atoi(argsu[2]), atoi(argsu[3]), atoi(argsu[4])); //
         }
         if (buf[0] == '{') {
 
