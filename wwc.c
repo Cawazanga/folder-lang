70dec9465c4a43a534d3542c76de93661d020cc1
Cawazanga <alexandrpav6@icloud.com>
2026-09-06 19:47:51 +0300

    Расширил поле ввода номера sbuf

--- wwc.c
+++ wwc.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include "func.h"
 void wwc(const char *csev) {
-    char fa[2];
+    char fa[3];
     char ta[8];
-    if (sscanf(csev, "%1s %7s", fa, ta) != 2) {
+    if (sscanf(csev, "%2s %7s", fa, ta) != 2) {
         return;
     }
     cycle(fa, atoi(ta));
