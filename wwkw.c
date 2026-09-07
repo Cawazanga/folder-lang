ff2387b940ba5ff7ac4bd9e346ea9c28f3955028
Cawazanga <alexandrpav6@icloud.com>
2026-09-07 07:32:07 +0300

    Убрал ошибку при которой при вводе ключевого слова оно выполнялось, но печаталась ошибка о том что данная коммада не найдена

--- wwkw.c
+++ wwkw.c
@@ -3,12 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 void wwkw(const char *keyword) {
-    if (strcmp(keyword, "sbuflist1") == 0)
-        printf("%s", sbuf[1]);
-    if (strcmp(keyword, "runsbuf1") == 0)
+    if (strcmp(keyword, "sbuflist") == 0) {
+        puts("In proccesing");
+    } else if (strcmp(keyword, "runsbuf1") == 0) {
         runcorbuf(sbuf[1], iv);
-    if (strcmp(keyword, "newsbuf") == 0)
-        puts("in proccesing\n");
-    else
-        puts("Error:not found keyword");
+    } else {
+        printf("Error: Not find this keyword \"%s\"\n", keyword);
+    }
 }
